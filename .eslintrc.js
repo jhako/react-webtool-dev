c899f2511da182484b957ea5b14d238d87717381
MASA <dummy@email>
2022-05-19 14:31:03 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'import/extensions': [
+      'error',
+      { extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'] },
+    ],
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# React ツール開発
+
+## 環境構築
+
+`npm init`
+
+## Eslint 実行
+
+`npx eslint . --ext .tsx --ext .ts`
